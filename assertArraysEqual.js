ad37e99294e454d70e683b0f1ccf264a55dee7a7
Robert Di Ninno <robertdininno@gmail.com>
2022-05-04 04:33:21 +0000

    console log is array is equal or not

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const eqArrays = function(a, b){
+  let isEqual = false;
+
+  if(a.length !== b.length){
+    return false;
+  }
+
+  for(let i = 0; i < a.length; i++){
+    if(a[i] === b[i]){
+      isEqual = true;
+    } else if (a[i] !== b[i]){
+      return false;
+    }
+  }
+
+  return isEqual;
+}
+
+const assertArraysEqual = function(a, b){
+  if(eqArrays(a, b)){
+    console.log("The arrays are equal");
+  } else {
+    console.log("The Arrays are not equal");
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3,]);
+assertArraysEqual([1, 2, 3], [1, 2, '3',]);
+assertArraysEqual([1, 2, 'three'], [1, 2, '3',]);
+assertArraysEqual([1, 2, 'three'], [1, 2, 'three',]);
