269cdeb8638b8ed229825f1d29269056d1be93b1
Robert Di Ninno <robertdininno@gmail.com>
2022-05-09 22:42:51 +0000

    test for assertEqualTests

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require(`../assertEqual`);
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(true, true);
+assertEqual("test", "test");
+assertEqual(5, 8);
+assertEqual(true, false);
+assertEqual("finished", "not");
