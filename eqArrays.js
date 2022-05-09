e43f714a6b9fcb3d9e341c88f4df0865e993678f
Robert Di Ninno <robertdininno@gmail.com>
2022-05-09 22:55:47 +0000

    small cleanup of extra text

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,8 @@
 const eqArrays = (a, b) => {
-  //if lengths different return false
   if (a.length !== b.length) {
     return false;
   }
 
-  //helper function to look through elements of array a
   for (const element in a) {
     if (Array.isArray(a[element]) && Array.isArray(b[element])) {
       if (!eqArrays(a[element], b[element])) {
