d6348ad8454f4f887eb20dfda87bd6a8d56f4046
Robert Di Ninno <robertdininno@gmail.com>
2022-05-09 21:30:08 +0000

    add recursion for nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,36 +7,47 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (a, b) => {
-  let isEqual = false;
-
+  //if lengths different return false
   if (a.length !== b.length) {
     return false;
   }
 
-  for (let i = 0; i < a.length; i++) {
-
-
-    if (a[i] === b[i]) {
-      isEqual = true;
-    } else if (a[i] !== b[i]) {
+  //helper function to look through elements of array a
+  for (const element in a) {
+    if (Array.isArray(a[element]) && Array.isArray(b[element])) {
+      if(!eqArrays(a[element], b[element])){
+        return false;
+      }
+    } else if (a[element] !== b[element]) {
       return false;
     }
   }
 
-  return isEqual;
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
+
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);// => false
+// assertEqual(eqArrays([1, [2], 3], [1, [2], 3]), true); // => should PASS
+// assertEqual(eqArrays([1, [2, 3]], [1, [2, 3]]), true); // => should PASS
+// assertEqual(eqArrays([1, [2, [3]]], [1, [2, [3]]]), true); // => should PASS
 
-assertEqual(eqArrays([1, 2, 3, 5], [1, 2, 3, 5]), true); // => true
-assertEqual(eqArrays([1, 2, 3, 5], [3, 2, 1]), true); // => false
+// assertEqual(eqArrays([1, [5], 3], [1, [2], 3]), false); // => FALSE
+// assertEqual(eqArrays([1, [2, 5]], [1, [2, 3]]), false); // => FALSE
+// assertEqual(eqArrays(['a', ['e'], ['c', ['d']]], ['a', ['b'], ['c', ['d']]]), false); // => FALSE
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
 
-assertEqual(eqArrays([1, 2, 'three'], [1, 2, 'three']), true); // => true
-assertEqual(eqArrays([1, 2, 'three'], [1, 2, 3]), true); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);// => false
+// assertEqual(eqArrays([1, 2, 3, 5], [1, 2, 3, 5]), true); // => true
+// assertEqual(eqArrays([1, 2, 3, 5], [3, 2, 1]), true); // => false
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+// assertEqual(eqArrays([1, 2, 'three'], [1, 2, 'three']), true); // => true
+// assertEqual(eqArrays([1, 2, 'three'], [1, 2, 3]), true); // => false
