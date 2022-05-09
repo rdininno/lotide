29a3f3dac09bdac484bd65edd4a1690771db08f9
Robert Di Ninno <robertdininno@gmail.com>
2022-05-09 22:45:32 +0000

    test for findKey

---
+++ test/findKeyTest.js
@@ -0,0 +1,39 @@
+const findKey = require(`../findKey`);
+const assertEqual = require(`../assertEqual`)
+
+//Tests
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 4 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 4), "Ora");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 4 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 5), undefined);
