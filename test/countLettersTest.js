8fc325f3a4b98e56d4ab486b42818a00dbfc3f06
Robert Di Ninno <robertdininno@gmail.com>
2022-05-09 22:44:01 +0000

    test for countLettersTest

---
+++ test/countLettersTest.js
@@ -0,0 +1,4 @@
+const countLetters = require(`../countLetters`);
+
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("this is a sentence to test this function out. :)"));
